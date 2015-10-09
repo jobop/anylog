b014f324c27f4c643b237f2443a669e008d4d9e4
jobop <zw19860913@163.com>
2015-10-09 15:58:39 +0800

    add annotations

---
+++ anylog-spi/src/main/java/com/github/jobop/anylog/spi/annotations/SpiDesc.java
@@ -0,0 +1,34 @@
+package com.github.jobop.anylog.spi.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 此注解可用于用户的spi实现之上，作为实现的描述和具体字段的描述
+ * 
+ * @author nevynzheng
+ * 
+ */
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface SpiDesc {
+	/**
+	 * spi描述
+	 * 
+	 * @return
+	 */
+	String desc();
+
+	/**
+	 * 是否可空
+	 * 
+	 * @return
+	 */
+	boolean canNull() default false;
+}
