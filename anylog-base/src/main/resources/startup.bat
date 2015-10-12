00e40d6e7d063846043ca26154cae543295e3cce
jobop <zw19860913@163.com>
2015-10-12 23:26:06 +0800

    修正windows下某些jdk版本无法加载attach的问题

--- anylog-base/src/main/resources/startup.bat
+++ anylog-base/src/main/resources/startup.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Djava.ext.dirs=lib\;providers\  -DappHome="conf"  -jar lib\${project.artifactId}-${project.version}.jar 52808
+%JAVA_HOME%\bin\java -Djava.ext.dirs=lib\;providers\ -Djava.library.path=%JAVA_HOME%\jre\bin  -DappHome="conf"  -jar lib\${project.artifactId}-${project.version}.jar 52808
 pause
