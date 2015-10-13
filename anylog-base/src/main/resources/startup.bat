ecd80e62c68ed55564a54b81ae2834139e35a427
jobop <zw19860913@163.com>
2015-10-13 16:13:41 +0800

    修正管理员权限执行时找不到路径的问题

--- anylog-base/src/main/resources/startup.bat
+++ anylog-base/src/main/resources/startup.bat
@@ -1,3 +1,4 @@
 @echo off
+cd /d %~dp0
 %JAVA_HOME%\bin\java -Djava.ext.dirs=lib\;providers\ -Djava.library.path=%JAVA_HOME%\jre\bin  -DappHome="conf"  -jar lib\${project.artifactId}-${project.version}.jar 52808
 pause
